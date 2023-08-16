b2656c089d8a17d9619e73c3172d7574e21e5a6f
GILBERTO QUIROGA <127807834+BARKLEYNS23@users.noreply.github.com>
2023-08-16 09:37:43 -0500

    add.php

--- modulos/materias/add.php
+++ modulos/materias/add.php
@@ -1,13 +1,15 @@
 <?php
-    session_start();
-    include_once("../../config/DBConect.php");
-    include_once("../../config/Config.php");    
+session_start();
+include_once("../../config/DBConect.php");
+include_once("../../config/Config.php");
 
-    if(isset($_POST['nombre']))      $nombre = $_POST['nombre']; 
+if(isset($_POST['nombre'])) {
+    $nombre = $_POST['nombre'];
+    $horas = $_POST['horas'];
+}
 
-    $conexion = new Database;  
-    $result = $conexion->CrearMateria($nombre);
+$conexion = new Database;  
+$result = $conexion->CrearMateria($nombre, $horas);
 
-    header("Location: ".ROOT."modulos/materias/materias.php?mensaje=".$result);
-
+header("Location: ".ROOT."modulos/materias/materias.php?mensaje=".$result);
 ?>
